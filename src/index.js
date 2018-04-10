c70c4f76eef8d7ce5ee9d35c897939594deded84
Gary Hall <guategary@gmail.com>
2018-04-10 09:10:37 -0400

    Interim commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import getMainSection from './mainTab';
+
 function createTabMenu() {
 	const menuItems = ['Main', 'Menu', 'Contact'];
 
---
+++ src/mainTab.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const getMainSection = () => {
+};
+
+export { getMainSection };
